f918ae6b3b2d70d8f1ad49f6d7038e309a68bcc8
Tanawin Jenkunnaphat <againnakub@hotmail.com>
2020-07-07 21:02:16 +0700

    save

---
+++ week1/Methods.py
@@ -0,0 +1,21 @@
+class Calculate_area:
+
+    def rectangle_area(self, w, h):
+        return w * h
+    
+    @classmethod
+    def triangle_area(cls, b, h):
+        return 0.5 * b * h
+
+    @staticmethod
+    def circle_area(r):
+        return 3.14 * r * r
+
+cal = Calculate_area()
+cal_rec = cal.rectangle_area(4,5)
+cal_tri = cal.triangle_area(4, 5)
+cal_circle = cal.circle_area(5)
+
+print('Rectangle Area = ' ,cal_rec)
+print('Triangle area = ', cal_tri)
+print('Circle Area = ',cal_circle)
