e54368d7ce1a3826e5a1fa2d24daace3348b2062
Tanawin Jenkunnaphat <againnakub@hotmail.com>
2020-07-14 21:19:16 +0700

    fin

--- week2/ex4.py
+++ week2/ex4.py
@@ -8,4 +8,21 @@ try:
 
     cursor = connection.cursor()
 
-    postgres_insert_query = '''
+    postgres_insert_query = ''' INSERT INTO students (student_id, f_name, l_name, e_mail) VALUES (%s,%s,%s,%s)'''
+    record_to_insert = ('3100796777241', 'Anirach',
+                        'Mignkhwan', 'Anirach.m@fitm.kmutnb.ac.th') 
+    cursor.execute(postgres_insert_query, record_to_insert)
+
+    connection.commit()
+    count = cursor.rowcount
+    print(count,'Record inserted successfully into students table')
+
+except (Exception, psycopg2.Error) as error:
+    if(connection):
+        print("Failed to insert record into students table", error)
+
+finally:
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
