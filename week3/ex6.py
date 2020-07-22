4a52c5e68115f10036963df38ee3d7112364db9a
INE-01 <INE-01@Admins-iMac-6.local>
2020-07-22 16:22:06 +0700

    done

---
+++ week3/ex6.py
@@ -0,0 +1,21 @@
+form flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+
+    return render_template('index.html', name=name, comment=comment)
+
+if __name__ == '__main__':
+    app.run(debug=True)
