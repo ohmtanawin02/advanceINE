8d56012cf422aa56486f5cb3632eb8a694ab672f
INE-01 <INE-01@Admins-iMac-6.local>
2020-09-16 16:47:15 +0700

    commit

--- aftermidterm/ORM-Database/Ex-12.py
+++ aftermidterm/ORM-Database/Ex-12.py
@@ -7,7 +7,7 @@ import uuid
 engine = sqlalchemy.create_engine('sqlite:///po.db')
 Base = declarative_base()
 
-class Order_Produck(Base):
+class Order_Product(Base):
     __tablename__ = 'order_product'
     id = Column(String(35), primary_key=True, unique=True)
     order_is = Column(Integer, ForeignKey('orders.id'), primary_key=True)
@@ -29,3 +29,90 @@ class Order_Produck(Base):
         return '<Order_Product {}>'.format(self.order.name+" "+self.product.name)
 
 class Product(Base):
+    __tablename__ = 'products'
+    id = Column(String(35), primary_key=True, unique= True)
+    name = Column(String(80), nullable=False)
+
+    orders = relationship("Order", secondary="order_product", viewonly=True)
+
+    def __init__(self, name):
+        self.id = uuid.uuid4().hex
+        self.name = name
+        self.orders = []
+
+    def __repr__(self):
+        return '<Product {}>'.format(self.name)
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(String(35), primary_key=True, unique=True)
+    name = Column(String(80), nullable=False)
+
+    products = relationship(
+        "Product", secondary="order_product", viewonly=True)
+    
+    def add_products(self, items):
+        for product, qty in items:
+            self.order_products.append(Order_Product(
+                order = self, product=product, quantity=qty))
+    
+    def __init__(self, name):
+        self.id = uuid.uuid4().hex
+        self.name = name
+        self.products = []
+
+    def __repr__(self):
+        return '<Order {}>'.format(self.name)
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bing=engine)
+session = Session()
+
+prod1 = Product(name="Oreo")
+prod2 = Product(name="Hide and Seek")
+prod3 = Product(name="Marie")
+prod4 = Product(name="Good Day")
+
+session.add_all([prod1,prod2,prod3,prod4])
+session.commit()
+
+order1 = Order(name="First Order")
+order2 = Order(name="Second Order")
+
+order1.add_products([(prod1, 4),(prod2,5),(prod3,4)])
+order2.add_products([(prod2, 6),(prod1,1),(prod3,2),(prod4,1)])
+
+session.commit()
+
+print("Products array of order1 : ")
+print(order1.products)
+print("Products array of order2 : ")
+print(order2.products)
+print("Orders array of prod1: ")
+print(prod1.orders)
+print("Orders array of prod2: ")
+print(prod2.orders)
+print("Orders array of prod3: ")
+print(prod3.orders)
+print("Orders array of prod4: ")
+print(prod4.orders)
+
+print("Order_Products Array of order1 : ")
+print(order1.order_products)
+
+print("Order_Products Array of prod1 : ")
+print(prod1.order_products)
+
+
+
+
+
+
+
+
+
+
+
+
