2b786d4ba1aa1663802ad966ea257045cdae155c
INE-01 <INE-01@Admins-iMac-6.local>
2020-09-23 17:00:03 +0700

    commit

--- aftermidterm/ORM-Database/DataTable/run.py
+++ aftermidterm/ORM-Database/DataTable/run.py
@@ -1,40 +1,38 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from datatables import column_dt, datatables
+from datatables import ColumnDT, DataTables
 import datetime
 import random
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sites.db'
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40))
-    phone = db.Column(db.String(10))
+    phone = db.Column(db.String(40))
     birthday = db.Column(db.Date)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-
 @app.route('/load_table_sql', methods=['GET', 'POST'])
 def load_table_sql():
     columns = [
-        column_dt(User.id),
-        column_dt(User.name),
-        column_dt(User.phone),
-        column_dt(User.birthday),
+        ColumnDT(User.id),
+        ColumnDT(User.name),
+        ColumnDT(User.phone),
+        ColumnDT(User.birthday),
     ]
     query = db.session.query().select_from(User)
-    rowTable = datatables.DataTables(request.args.to_dict(), query, columns)
-    # returns what is needed by DataTable
-    return rowTable.output_result()
+    rowTable = DataTables(request.args.to_dict(), query, columns)
 
+    #return what is needed by DataTable
+    return rowTable.output_result()
 
 def random_date():
     start_date = datetime.date(1997, 1, 1)
@@ -44,9 +42,8 @@ def random_date():
     days_between_dates = time_between_dates.days
     random_number_of_days = random.randrange(days_between_dates)
     random_date = start_date + datetime.timedelta(days=random_number_of_days)
-
+    
     return random_date
-
 
 def create_db_user():
     db.create_all()
@@ -58,6 +55,5 @@ def create_db_user():
     print('create done')
     show_db_user()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
